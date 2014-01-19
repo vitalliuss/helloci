53aa7ab20894b304c23e94ce5756b2c612a68be7
Vitali Shulha <vitali_shulha@epam.com>
2014-01-19 20:14:41 +0300

    add base project

---
+++ src/main/java/com/github/vitalliuss/helloci/App.java
@@ -0,0 +1,13 @@
+package com.github.vitalliuss.helloci;
+
+/**
+ * Hello CI!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello CI!" );
+    }
+}
---
+++ src/test/java/com/github/vitalliuss/helloci/AppTest.java
@@ -0,0 +1,28 @@
+package com.github.vitalliuss.helloci;
+
+
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void testShoudBePassed() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testShouldBeFailed() {
+		assertTrue(false);
+	}
+
+	@Ignore("Not implemented yet")
+	@Test
+	public void testShouldBeSKipped() {
+		assertTrue(true);
+	}
+}
