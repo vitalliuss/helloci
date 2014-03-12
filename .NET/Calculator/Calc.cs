f30577716b2d516f79b3055f2a3a6ae040e4e870
Vitali Shulha <vitali_shulha@epam.com>
2014-03-12 15:07:55 +0300

    add dotnet project

---
+++ .NET/Calculator/Calc.cs
@@ -0,0 +1,26 @@
+﻿namespace Calculator
+{
+
+    public class Calc
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+        
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        
+        public static int Devide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ .NET/CalculatorTest/TestCalculator.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+using Calculator;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class TestCalculator
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Starting TestHelloWorld tests");
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("TestHelloWorld tests are finished");
+        }
+
+
+        [Category("SumTests")]
+        [Test]
+        public void OneCanSumPositiveIntegers()
+        {
+            Assert.That(Calc.Sum(5, 5), Is.EqualTo(10));
+        }
+
+        [Category("SumTests")]
+        [Test]
+        public void OneCanSumNegativeIntegers()
+        {
+            Assert.That(Calc.Sum(-5, -5), Is.EqualTo(-10));
+        }
+
+        [Category("SubstractTests")]
+        [Test]
+        public void OneCanSubstractPositiveIntegers()
+        {
+            Assert.That(Calc.Substract(5, 5), Is.EqualTo(0));
+        }
+
+        [Category("SubstractTests")]
+        [Test]
+        public void OneCanSubstractNegativeIntegers()
+        {
+            Assert.That(Calc.Substract(-5, -5), Is.EqualTo(0));
+        }     
+        
+        [Category("SubstractTests")]
+        [Test]
+        public void OneCanSubstracPositiveIntegertFromZero()
+        {
+            Assert.That(Calc.Substract(0, 100), Is.EqualTo(-100));
+        }
+
+        [Category("SubstractTests")]
+        [Test]
+        public void OneCanSubstracNegativeIntegertFromZero()
+        {
+            Assert.That(Calc.Substract(0, -100), Is.EqualTo(99));
+        }
+        
+        [Ignore("Ignoring")]
+        [Category("SubstractTests")]
+        [Test]
+        public void OneCanDevideByZero()
+        {
+            Assert.That(Calc.Devide(1, 0), Is.EqualTo(0));
+        }         
+    }
+}
